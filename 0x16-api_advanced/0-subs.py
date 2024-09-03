e8347ffeb09ea818b3e6216c438c864cdff7db29
ThokozaniLegend <teejey2@gmail.com>
2024-09-03 14:09:19 +0000

    subs2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 
-import requests
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
@@ -9,21 +8,20 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        
+
         # Check for a 404 status code which indicates that the subreddit doesn't exist
-        if response.status_code == 404:
+        if response.status_code != 200:
             return 0
-        
+
         # Parse the response and safely access the data
-        results = response.json().get("data")
-        if results:
-            return results.get("subscribers", 0)  # Return 0 if 'subscribers' key is missing
-        
-        return 0  # Return 0 if "data" key is missing in the JSON response
-    
+        results = response.json().get("data", {})
+        return results.get("subscribers", 0)  # Return 0 if 'subscribers' key is missing
+
     except requests.RequestException:
         # Return 0 if there is any request-related error (network issues, etc.)
         return 0
+
+
