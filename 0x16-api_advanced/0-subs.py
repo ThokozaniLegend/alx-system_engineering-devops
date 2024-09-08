8fa60b183589cc01edf4dd7a3fc5ac64b269ad28
ThokozaniLegend <teejey2@gmail.com>
2024-09-08 03:56:54 -0700

    top10 update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,17 +8,19 @@ import requests
 def number_of_subscribers(subreddit):
     """
     Get the number of subscribers for a given subreddit.
+    Parameters:
+    subreddit (str): The name of the subreddit to query.
     Returns:
     int: Number of subscribers or 0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'user-agent': 'request'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json().get("data", {})
+            return data.get("subscribers", 0)
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-
-    data = response.json().get("data")
-    num_subs = data.get("subscribers", 0)  # Handle missing 'subscribers'
-
-    return num_subs
