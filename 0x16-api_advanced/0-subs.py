3530429bbcf41c2446cd77086b4b7109759a537d
ThokozaniLegend <teejey2@gmail.com>
2024-09-08 03:36:22 -0700

    top10 update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """
     Get the number of subscribers for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'user-agent': 'request'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
